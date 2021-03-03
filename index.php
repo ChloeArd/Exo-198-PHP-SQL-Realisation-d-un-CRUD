7bd269d826ab967e28a916d813f152cb09fa537f
ChloeArd <chloe.ardoise@gmail.com>
2021-03-03 11:02:02 +0100

    Exercice 198

--- index.php
+++ index.php
@@ -10,11 +10,11 @@ try {
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION).
     $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    create("Ard", "Chloé", 18, $bdd);
+    create("Ardoise", "Chloé", 18, $bdd);
     create("Fraise", "Gertrude", 42, $bdd);
     reade($bdd);
-    update("Gertrude", "Fraise", 32, 2, $bdd);
-    delete(1, $bdd);
+    update("Gertrude", "Ananas", 32, 8, $bdd);
+    delete(7, $bdd);
 
 
 
@@ -25,9 +25,10 @@ catch (PDOException $e) {
 
 
 function create($nom, $prenom, $age, $bdd) {
-    $sql = ("INSERT INTO eleves VALUES (null, $nom, $prenom, $age)");
+    $sql = ("INSERT INTO eleves VALUES (null, '$nom', '$prenom', $age)");
     $bdd->exec($sql);
 }
+
 
 function reade($bdd) {
     $stmt = $bdd->prepare("SELECT * from eleves");
@@ -36,10 +37,11 @@ function reade($bdd) {
 
     if ($state) {
         foreach ($stmt -> fetchAll() as $user) {
-            echo "Eleves " . $user['id'] . ": " . $user['nom'] . " " . $user['prenom'] . ", " . $user['age'] . " ans.";
+            echo "Eleves " . $user['id'] . ": " . $user['nom'] . " " . $user['prenom'] . ", " . $user['age'] . " ans. <br>";
         }
     }
 }
+
 
 function update($prenom, $nom, $age, $idEleve, $bdd) {
     $stm = $bdd->prepare("UPDATE eleves SET prenom = :prenom, nom = :nom, age = :age WHERE id = :id");
@@ -52,10 +54,11 @@ function update($prenom, $nom, $age, $idEleve, $bdd) {
     $stm->execute();
 }
 
+
 function delete($idEleve, $bdd) {
     $sql = "DELETE FROM eleves WHERE id = $idEleve";
     if ($bdd->exec($sql) !== false) {
-        echo "L'élève ayant l'id" . $idEleve . " a bien été supprimé !";
+        echo "L'élève ayant l'id " . $idEleve . " a bien été supprimé !";
     }
 
 }
